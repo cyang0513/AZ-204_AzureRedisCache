488025ec7868743249e39cfa595eacc570d38de0
Chengkai Yang <yangchengkai@gmail.com>
2021-03-01 23:32:16 +0100

    managed identity to access app config

--- AzureRedisCache/Program.cs
+++ AzureRedisCache/Program.cs
@@ -24,12 +24,17 @@ namespace AzureRedisCache
                  webBuilder.ConfigureAppConfiguration(az => {
                     var cb = az.Build();
                     az.AddAzureAppConfiguration(x=> {
-                       x.Connect(cb.GetConnectionString("AppConfig"));
+                       
+                       x.Connect(new Uri("https://chyaappconfig.azconfig.io"), new ManagedIdentityCredential());
 
-                       //Setup service principle to access key vault //appConfigSp
-                       Environment.SetEnvironmentVariable("AZURE_CLIENT_ID ", "7f5e090c-9557-47c8-930f-0a973e854f2c");
-                       Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "Hh1Ji4oOLWuTa-a30RM1gAI7vlVTjD~fvO");
-                       Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "4e6f57dc-a3d9-4a0c-818b-a7c1bb2b79f6");
+                       //Local:
+                       //x.Connect(cb.GetConnectionString("AppConfig"));
+
+                       //Local: Setup service principle to access key vault //appConfigSp
+                       //Environment.SetEnvironmentVariable("AZURE_CLIENT_ID ", "7f5e090c-9557-47c8-930f-0a973e854f2c");
+                       //Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "Hh1Ji4oOLWuTa-a30RM1gAI7vlVTjD~fvO");
+                       //Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "4e6f57dc-a3d9-4a0c-818b-a7c1bb2b79f6");
+
                        x.ConfigureKeyVault(kv => {
                           kv.SetCredential(new DefaultAzureCredential());
                           });
