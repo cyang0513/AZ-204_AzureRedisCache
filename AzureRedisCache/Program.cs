a5d97db49dfd58594bedf4896dc843e401eb1c4d
Chengkai Yang <yangchengkai@gmail.com>
2021-03-02 11:49:31 +0100

    //Allow managed identity to access app configuration and KV when deployed

--- AzureRedisCache/Program.cs
+++ AzureRedisCache/Program.cs
@@ -23,21 +23,22 @@ namespace AzureRedisCache
               {
                  webBuilder.ConfigureAppConfiguration(az => {
                     //var cb = az.Build();
-                    az.AddAzureAppConfiguration(x=> {
-                       
-                       x.Connect(new Uri("https://chyaappconfig.azconfig.io"), new DefaultAzureCredential());
+                    az.AddAzureAppConfiguration(x=>
+                                                {
+                                                   //Allow managed identity to access app configuration and KV when deployed
+                                                   var cred = new DefaultAzureCredential();
 
-                       //Local:
-                       //x.Connect(cb.GetConnectionString("AppConfig"));
+                                                   x.Connect(new Uri("https://chyaappconfig.azconfig.io"), cred);
 
-                       //Local: Setup service principle to access key vault //appConfigSp
-                       //Environment.SetEnvironmentVariable("AZURE_CLIENT_ID ", "7f5e090c-9557-47c8-930f-0a973e854f2c");
-                       //Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "Hh1Ji4oOLWuTa-a30RM1gAI7vlVTjD~fvO");
-                       //Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "4e6f57dc-a3d9-4a0c-818b-a7c1bb2b79f6");
+                                                   //Local:
+                                                   //x.Connect(cb.GetConnectionString("AppConfig"));
 
-                       x.ConfigureKeyVault(kv => {
-                          kv.SetCredential(new DefaultAzureCredential());
-                          });
+                                                   //Local: Setup service principle to access key vault //appConfigSp
+                                                   //Environment.SetEnvironmentVariable("AZURE_CLIENT_ID ", "7f5e090c-9557-47c8-930f-0a973e854f2c");
+                                                   //Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", "Hh1Ji4oOLWuTa-a30RM1gAI7vlVTjD~fvO");
+                                                   //Environment.SetEnvironmentVariable("AZURE_TENANT_ID", "4e6f57dc-a3d9-4a0c-818b-a7c1bb2b79f6");
+
+                                                   x.ConfigureKeyVault(kv => { kv.SetCredential(cred); });
                     });
                  });
                  webBuilder.UseStartup<Startup>();
